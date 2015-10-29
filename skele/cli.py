541f6cd13fe0899a2c415c64931a6ca4a8485205
Randall Degges <r@rdegges.com>
2015-10-29 16:52:14 -0700

    Adding a dynamic CLI entrypoint.

---
+++ skele/cli.py
@@ -0,0 +1,42 @@
+"""
+skele
+
+Usage:
+  skele hello
+  skele -h | --help
+  skele --version
+
+Options:
+  -h --help                         Show this screen.
+  --version                         Show version.
+
+Examples:
+  skele hello
+
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/rdegges/skele-cli
+"""
+
+
+from inspect import getmembers, isclass
+
+from docopt import docopt
+
+from . import __version__ as VERSION
+
+
+def main():
+    """Main CLI entrypoint."""
+    import commands
+    options = docopt(__doc__, version=VERSION)
+
+    # Here we'll try to dynamically match the command the user is trying to run
+    # with a pre-defined command class we've already created.
+    for k, v in options.iteritems():
+        if hasattr(commands, k):
+            module = getattr(commands, k)
+            commands = getmembers(module, isclass)
+            command = [command[1] for command in commands if command[0] != 'Base'][0]
+            command = command(options)
+            command.run()
